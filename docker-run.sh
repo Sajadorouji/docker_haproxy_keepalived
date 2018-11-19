cd01f5b1d68865be0b1cb6737ea032aeb6891365
sajadorouji <sajad.orj@gmail.com>
2018-11-19 15:31:09 +0330

    Modify docker-run.sh file.

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,4 @@ docker run -d --name haproxy \
 -v $PWD/etc/haproxy/:/usr/local/etc/haproxy \
 -v $PWD/etc/keepalived/:/usr/local/etc/keepalived \
 --network host --cap-add=NET_ADMIN \
-containername
+IMAGENAME
